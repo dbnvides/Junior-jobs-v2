6c37e8b2fd28f5b92e329d5110393ca4ecb1e761
Dionisio <benevidescursos@gmail.com>
2023-01-04 17:37:08 -0300

    fix: readjusting header size

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { Header } from "./components/Header";
 import { Global } from "./styles/global";
 
 function App() {
-    return (
-        <>
-          <Global />
-        </>
-    );
+  return (
+    <>
+      <Global />
+      <Header />
+    </>
+  );
 }
 
 export default App;
