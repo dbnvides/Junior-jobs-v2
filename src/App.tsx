a73dcac23965111b9d3572898bd008bb7103c4fe
Leonardo L Cunha <n7@MacBook-Air-de-Leonardo.local>
2023-01-05 15:44:04 -0300

    feat/loginPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { MainRoutes as Routes } from "./routes";
 import { Global } from "./styles/global";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
+    <ToastContainer
+      position="top-right"
+      autoClose={2000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
       <Global />
       <Routes />
     </>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Login } from "../pages/login";
 import { Company } from "../pages/company";
+import { Register } from "../pages/register";
 
 export const MainRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/register" element={<h1>register</h1>} />
+      <Route path="/register" element={<Register/>} />
       <Route path="/company" element={<Company />} />
     </Routes>
   );
