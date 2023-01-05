0600eb59c44cce690bdf2770171da060706abf2a
Valdecir Teixeira <vardeba@hotmail.com>
2023-01-05 16:44:14 -0300

    fix: modal responsivity

--- src/components/ModalAddJob/styles.ts
+++ src/components/ModalAddJob/styles.ts
@@ -1,16 +1,31 @@
 import styled from "styled-components";
 
 export const StyledModalAddJob = styled.div`
-    width: 539px;
+    width: 350px;
     max-width: 100%;
+    margin-top: 3rem;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    @media (min-width: 769px) {
+        width: 539px;
+    }
 
     .modalAddJobContainer {
+        width: 90%;
         display: flex;
         flex-direction: column;
+        max-height: 400px;
+        overflow-y: scroll;
+        scrollbar-width: 0;
+        @media (min-width: 769px) {
+            max-height: 600px;
+        }
+
+        ::-webkit-scrollbar {
+            width: 0;
+        }
 
         form {
             width: 100%;
@@ -25,6 +40,7 @@ export const StyledModalAddJob = styled.div`
             background-color: var(--color-primary);
             color: var(--white);
             border-radius: var(--radius-1);
+            margin-bottom: 2rem;
             transition: 0.2s ease;
 
             :hover {
