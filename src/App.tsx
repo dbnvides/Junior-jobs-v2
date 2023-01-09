4cf4201a3965105fed258e3f05dae9d62f74772c
Gabriel de Lima Santana <gabr.lima7@gmail.com>
2023-01-09 12:38:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { ContextoProvider } from "./contexts/HomeContext";
-import { Home } from "./pages/home";
+import { MainRoutes as Routes } from "./routes";
 import { Global } from "./styles/global";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import { Routes } from "react-router-dom";
-import { MainRoutes } from "./routes";
+
 
 function App() {
   return (
     <>
-      <Global />
       <ToastContainer
         position="top-right"
         autoClose={2000}
@@ -22,7 +19,8 @@ function App() {
         pauseOnHover
         theme="colored"
       />
-      <MainRoutes />
+      <Global />
+      <Routes />
     </>
   );
 }
