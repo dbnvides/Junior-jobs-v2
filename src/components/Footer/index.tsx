24c98fbd8f503cbef53f7ef13d63ed903af5b50f
Valdecir Teixeira <vardeba@hotmail.com>
2023-01-10 14:56:59 -0300

    fix footer height, fix responsivity, fix font-family

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,26 +3,26 @@ import { Footer } from "./styles";
 import { SiInstagram, SiFacebook, SiTwitter } from "react-icons/si";
 
 export const StyledFooter = () => {
-  return (
-    <Footer>
-      <div className="footerContainer">
-        <div className="socialMediaIcons">
-          <a href="#">
-            <SiInstagram size={30} color="white" />
-          </a>
-          <a href="#">
-            <SiFacebook size={30} color="white" />
-          </a>
-          <a href="#">
-            <SiTwitter size={30} color="white" />
-          </a>
-        </div>
-        <div className="logoContainerFooter">
-          <a href="#">
-            <span>Júnior Jobs</span>
-          </a>
-        </div>
-      </div>
-    </Footer>
-  );
+    return (
+        <Footer>
+            <div className="footerContainer">
+                <div className="socialMediaIcons">
+                    <a href="#">
+                        <SiInstagram size={25} color="white" />
+                    </a>
+                    <a href="#">
+                        <SiFacebook size={25} color="white" />
+                    </a>
+                    <a href="#">
+                        <SiTwitter size={25} color="white" />
+                    </a>
+                </div>
+                <div className="logoContainerFooter">
+                    <a href="#">
+                        <span>Júnior Jobs</span>
+                    </a>
+                </div>
+            </div>
+        </Footer>
+    );
 };
