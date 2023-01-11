ba0ce084ef8eb39382878888693de0279d3f2f65
Carlos Eduardo <carlospcavalari@gmail.com>
2023-01-11 00:27:03 -0300

    feat: mobileFilterBar

--- src/components/CardJob/CardJob.tsx
+++ src/components/CardJob/CardJob.tsx
@@ -4,10 +4,10 @@ import { StyledJob } from "../../pages/home/style";
 import { api } from "../../services/api";
 
 export const CardJob = ({ elem }: any) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [user, setUser]: any = useState();
   const token = localStorage.getItem("@TOKEN");
-  const navigate = useNavigate();
+  console.log(user);
   useEffect(() => {
     const getUsers = async () => {
       try {
@@ -25,22 +25,20 @@ export const CardJob = ({ elem }: any) => {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <StyledJob onClick={() => navigate(`/job/${elem.job_name}`)}>
-=======
-    <StyledJob onClick={()=>{
-      localStorage.setItem("@JOBID", elem.id)
-      localStorage.setItem("@COMPANYID", elem.usersId)
-      navigate(`/job/${elem.job_name}`)}}>
-
->>>>>>> adbda727769bf758fabd0eac1b4d177369a0bad3
+    <StyledJob
+      onClick={() => {
+        localStorage.setItem("@JOBID", elem.id);
+        localStorage.setItem("@COMPANYID", elem.usersId);
+        navigate(`/job/${elem.job_name}`);
+      }}
+    >
       <div>
         <img src={user?.avatar} alt="logo da empresa" />
         <p>
           1m atrás • <span>{elem.period}</span>
         </p>
         <h2>{elem.job_name}</h2>
-        <p>{elem.description}</p>
+        <p className="description">{elem.description}</p>
       </div>
       <h4>Busca cidade ai</h4>
     </StyledJob>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,6 +6,7 @@ import { CardJob } from "../../components/CardJob/CardJob";
 import searchIcon from "../../assets/img/search-icon.png";
 import localizationIcon from "../../assets/img/localization-icon.png";
 import funnelIcon from "../../assets/img/funnel-icon.png";
+import closeIcon from "../../assets/img/close-icon.png";
 import { FilterBar } from "./style";
 import { HomeContext } from "../../contexts/HomeContext";
 import { useContext, useEffect, useState } from "react";
@@ -24,6 +25,7 @@ export const Home = () => {
   }: any = useContext(HomeContext);
   const [pageCounter, setPageCounter] = useState(12);
   const [check, setCheck] = useState(false);
+  const [mobileFilter, setMobileFilter] = useState(false);
 
   useEffect(() => {
     const getJobs = async () => {
@@ -41,7 +43,9 @@ export const Home = () => {
   console.log(jobsList);
   const renderAllJobs = () => {
     return jobsList.map((elem: any, index: any) => {
-      return index + 1 <= pageCounter && <CardJob elem={elem} key={elem["id"]} />;
+      return (
+        index + 1 <= pageCounter && <CardJob elem={elem} key={elem["id"]} />
+      );
     });
   };
   const renderTitleFilter = () => {
@@ -49,15 +53,19 @@ export const Home = () => {
       return elem.job_name.toLowerCase().includes(searchTitle.toLowerCase());
     });
     return filteredJobs.map((elem: any, index: any) => {
-      return index + 1 <= pageCounter && <CardJob elem={elem} key={elem["id"]} />;
+      return (
+        index + 1 <= pageCounter && <CardJob elem={elem} key={elem["id"]} />
+      );
     });
   };
   const renderFullTime = () => {
     const filteredJobs = jobsList.filter((elem: any) => {
-      return elem.period === "integral";
+      return elem.period === "Integral";
     });
     return filteredJobs.map((elem: any, index: any) => {
-      return index + 1 <= pageCounter && <CardJob elem={elem} key={elem["id"]} />;
+      return (
+        index + 1 <= pageCounter && <CardJob elem={elem} key={elem["id"]} />
+      );
     });
   };
 
@@ -67,41 +75,83 @@ export const Home = () => {
         <Header />
         <Container>
           <FilterBar>
-            <div className="filterTitle">
-              <img src={searchIcon} alt="" />
-              <input
-                type="text"
-                placeholder="Filtrar por título..."
-                onChange={(e) => setSearchTitle(e.target.value)}
-              />
-            </div>
-            <div className="filterLocal">
-              <img src={localizationIcon} alt="" />
-              <input
-                type="text"
-                placeholder="Filtrar por localidade..."
-                onChange={(e) => {
-                  setSearchLocal(e.target.value);
-                }}
-              />
-            </div>
-            <div className="searchContainer">
-              <div>
-                <label>
-                  <input
-                    type="checkbox"
-                    checked={check}
-                    onChange={() => (check === false ? setCheck(true) : setCheck(false))}
-                  />
-                  Apenas Integral
-                </label>
-              </div>
-              <img src={funnelIcon} alt="" />
-              <button className="mobileSearch" onClick={() => renderTitleFilter()}>
+            <div className="mainFilter">
+              <div className="filterTitle">
                 <img src={searchIcon} alt="" />
-              </button>
-              <button onClick={() => renderTitleFilter()}>Procurar</button>
+                <input
+                  type="text"
+                  placeholder="Filtrar por título..."
+                  onChange={(e) => setSearchTitle(e.target.value)}
+                />
+              </div>
+              <div className="filterLocal">
+                <img src={localizationIcon} alt="" />
+                <input
+                  type="text"
+                  placeholder="Filtrar por localidade..."
+                  onChange={(e) => {
+                    setSearchLocal(e.target.value);
+                  }}
+                />
+              </div>
+              <div className="searchContainer">
+                <div>
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={check}
+                      onChange={() =>
+                        check === false ? setCheck(true) : setCheck(false)
+                      }
+                    />
+                    Apenas Integral
+                  </label>
+                </div>
+                {mobileFilter ? (
+                  <img
+                    src={closeIcon}
+                    alt=""
+                    onClick={() => {
+                      setMobileFilter(false);
+                    }}
+                  />
+                ) : (
+                  <img
+                    src={funnelIcon}
+                    alt=""
+                    onClick={() => {
+                      setMobileFilter(true);
+                    }}
+                  />
+                )}
+              </div>
             </div>
+            {mobileFilter && (
+              <div className="mobileFilter">
+                <div className="mobileLocalContainer">
+                  <img src={localizationIcon} alt="" />
+                  <input
+                    type="text"
+                    placeholder="Filtrar por localidade..."
+                    onChange={(e) => {
+                      setSearchLocal(e.target.value);
+                    }}
+                  />
+                </div>
+                <div className="mobileFulltimeContainer">
+                  <label>
+                    <input
+                      type="checkbox"
+                      checked={check}
+                      onChange={() =>
+                        check === false ? setCheck(true) : setCheck(false)
+                      }
+                    />
+                    Apenas Integral
+                  </label>
+                </div>
+              </div>
+            )}
           </FilterBar>
           <StyledJobsList>
             {check
@@ -112,7 +162,9 @@ export const Home = () => {
           </StyledJobsList>
         </Container>
         <StyledViewMoreContainer>
-          <button onClick={() => setPageCounter(pageCounter + 12)}>Carregar Mais</button>
+          <button onClick={() => setPageCounter(pageCounter + 12)}>
+            Carregar Mais
+          </button>
         </StyledViewMoreContainer>
         <StyledFooter />
       </StyledHome>
